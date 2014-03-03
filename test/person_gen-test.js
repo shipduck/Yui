549788e2bc0c187cc3539c4a74034f7d95448421
if1live <libsora25@gmail.com>
2014-03-03 23:42:55 +0900

    민번생성기 추가

--- test/person_gen-test.js
+++ test/person_gen-test.js
@@ -1,5 +1,6 @@
 if(typeof window === 'undefined') {
   assert = require('assert');
+  base = process.env.EXPRESS_COV ? require('../lib-cov/base.js') : require('../lib/base.js');
   person_gen = process.env.EXPRESS_COV ? require('../lib-cov/person_gen.js') : require('../lib/person_gen.js');
 }
 
@@ -105,6 +106,75 @@ describe('NameGenerator', function() {
       var gen = new person_gen.NameGenerator();
       var actual = gen();
       //console.log(actual);
+    })
+  })
+})
+
+describe('ssn', function() {
+  describe('#str2numList', function() {
+    it('run', function() {
+      var input = '12-a93s';
+      var output = [1, 2, 9, 3];
+      assert.deepEqual(output, person_gen.str2numList(input));
+    })
+  })
+})
+
+describe('SSNFilter', function() {
+  describe('#apply', function() {
+    it('run', function() {
+      var filter = new person_gen.SSNFilter();
+      assert.equal('123*', filter.apply('1234'));
+      assert.equal('12**', filter.apply('1234', 2));
+      assert.equal('****', filter.apply('1234', 4));
+      assert.equal('****', filter.apply('1234', 10));
+      assert.equal('1234', filter.apply('1234', -1));
+    })
+  })
+})
+
+describe('SSNValidator', function() {
+  describe('#validate', function() {
+    it('success', function() {
+      // 수동으로 생성한 샘플
+      var dataList = ['901212-1023457'];
+      var validator = new person_gen.SSNValidator();
+      for(var i = 0 ; i < dataList.length ; ++i) {
+        assert.equal(true, validator.validate(dataList[i]));
+      }
+    })
+    it('fail', function() {
+      var dataList = [
+        '111111-111111',  // 6-6 형태로 입력되서 맞지 않는 경우
+        '1111111111111',  // - 없이 입력한 경우
+        '901212-1023456',
+      ];
+      var validator = new person_gen.SSNValidator();
+      for(var i = 0 ; i < dataList.length ; ++i) {
+        assert.equal(false, validator.validate(dataList[i]));
+      }
+    })
+  })
+})
+
+describe('SSNGenerator', function() {
+  describe('#generate', function() {
+    it('run', function() {
+      var birthGen = new person_gen.BirthdayGenerator();
+      var data = birthGen();
+      var genderGen = new person_gen.GenderGenerator();
+      data['gender'] = genderGen();
+
+      console.log(data);
+
+      var generator = new person_gen.SSNGenerator(data);
+      var validator = new person_gen.SSNValidator();
+
+      for(var i = 0 ; i < 10 ; ++i) {
+        var val = generator();
+        //console.log(val);
+        assert.equal(true, validator.validate(val));
+      }
     })
   })
 })
